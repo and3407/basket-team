d3623a4e228c1bf7b22bd883f2de427151e6aa51
and3407 <and3407@yandex.ru>
2022-07-07 08:33:35 +0400

    add AllArgsConstructor

--- src/main/java/com/basketteamapi/basketteam/components/team/TeamSize.java
+++ src/main/java/com/basketteamapi/basketteam/components/team/TeamSize.java
@@ -1,18 +1,13 @@
 package com.basketteamapi.basketteam.components.team;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Builder
 @Data
+@AllArgsConstructor
 public class TeamSize {
     private int quantityActivePlayers;
     private int quantityReservePlayers;
-
-    public TeamSize() {}
-
-    public TeamSize(int quantityActivePlayers, int quantityReservePlayers) {
-        this.quantityActivePlayers = quantityActivePlayers;
-        this.quantityReservePlayers = quantityReservePlayers;
-    }
 }
